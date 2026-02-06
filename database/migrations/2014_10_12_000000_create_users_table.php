82fbfbdc856745336733a28c398e8a0f5a576c0f
hanasbp <hanasoke@gmail.com>
2026-02-06 15:12:37 +0700

    create migrations for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo');
+            $table->string('roles');
+            $table->string('access');
+            $table->string('birthdate');
             $table->rememberToken();
             $table->timestamps();
         });
