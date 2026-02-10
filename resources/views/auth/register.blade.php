d102ec3fc57dfe81f0932a3f6fa961ce21ebd973
hanasbp <hanasoke@gmail.com>
2026-02-10 18:32:16 +0700

    updated register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,45 +12,52 @@
         <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
           <div class="row g-3">
             <div class="col-md-6">
-              <label class="form-label">Full Name</label>
-              <input type="text" name="name" class="form-control is-invalid"> 
+              <label class="form-label" for="name" >Full Name</label>
+              <input type="text" name="name" id="name" class="form-control is-invalid"> 
               <div class="invalid-feedback">
                 Full Name Cannot be Null or Duplicate Name 
               </div>
             </div>
             <div class="col-md-6">
-              <label class="form-label">Email</label>
-              <input type="email" name="email" class="form-control is-invalid">
+              <label class="form-label" for="username">Username</label>
+              <input type="text" name="username" id="username" class="form-control is-invalid"> 
+              <div class="invalid-feedback">
+                Username Cannot be Null or Duplicate Name 
+              </div>
+            </div>
+            <div class="col-md-6">
+              <label class="form-label" for="email">Email</label>
+              <input type="email" name="email" id="email" class="form-control is-invalid">
               <div class="invalid-feedback">
                 Email Cannot be Null or Duplicate Email  
               </div>
             </div>
 
             <div class="col-md-6">
-              <label class="form-label">Password</label>
-              <input type="password" name="password" class="form-control is-invalid">
+              <label class="form-label" for="password">Password</label>
+              <input type="password" name="password" id="password" class="form-control is-invalid">
               <div class="invalid-feedback">
                 Password Cannot be Null   
               </div>
             </div>
             <div class="col-md-6">
-              <label class="form-label">Confirm Password</label>
-              <input type="password" name="password_confirmation" class="form-control is-invalid">
+              <label class="form-label" for="re_password">Confirm Password</label>
+              <input type="password" name="re_password" id="re_password" class="form-control is-invalid">
               <div class="invalid-feedback">
                 Password Cannot be Null   
               </div>
             </div>
 
             <div class="col-md-6">
-              <label class="form-label">Date of Birth</label>
-              <input type="date" name="birthdate" class="form-control is-invalid">
+              <label class="form-label" for="birthdate">Date of Birth</label>
+              <input type="date" name="birthdate" id="birthdate" class="form-control is-invalid">
               <div class="invalid-feedback">
                   Date of Birth Cannot be Null   
               </div>
             </div>
 
             <div class="col-md-6">
-              <label class="form-label">Phone Number</label>
+              <label class="form-label" for="phoneNumber">Phone Number</label>
               <input type="text" name="phone" class="form-control is-invalid" id="phoneNumber">
               <div class="invalid-feedback">
                   Phone Cannot be Null   
@@ -58,8 +65,8 @@
             </div>
 
             <div class="col-md-6">
-              <label class="form-label">Photo</label>
-              <input type="file" name="photo" class="form-control is-invalid" placeholder="Input Your Photo">
+              <label class="form-label" for="photo">Photo</label>
+              <input type="file" name="photo" id="photo" class="form-control is-invalid" placeholder="Input Your Photo">
               <div class="invalid-feedback">
                   Phone Cannot be Null   
               </div>
