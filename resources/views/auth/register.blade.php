0851d6233fb839253d261bda4861bdabdba2a8ae
hanasbp <hanasoke@gmail.com>
2026-02-10 18:10:24 +0700

    updated register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div class="container py-5">
   <div class="row g-4 justify-content-center">
-
     <!-- REGISTER -->
     <div class="col-lg-7">
       <div class="card auth-card p-4">
@@ -14,35 +13,56 @@
           <div class="row g-3">
             <div class="col-md-6">
               <label class="form-label">Full Name</label>
-              <input type="text" name="name" class="form-control" required>
+              <input type="text" name="name" class="form-control is-invalid"> 
+              <div class="invalid-feedback">
+                Full Name Cannot be Null or Duplicate Name 
+              </div>
             </div>
             <div class="col-md-6">
               <label class="form-label">Email</label>
-              <input type="email" name="email" class="form-control" required>
+              <input type="email" name="email" class="form-control is-invalid">
+              <div class="invalid-feedback">
+                Email Cannot be Null or Duplicate Email  
+              </div>
             </div>
 
             <div class="col-md-6">
               <label class="form-label">Password</label>
-              <input type="password" name="password" class="form-control" required>
+              <input type="password" name="password" class="form-control is-invalid">
+              <div class="invalid-feedback">
+                Password Cannot be Null   
+              </div>
             </div>
             <div class="col-md-6">
               <label class="form-label">Confirm Password</label>
-              <input type="password" name="password_confirmation" class="form-control">
+              <input type="password" name="password_confirmation" class="form-control is-invalid">
+              <div class="invalid-feedback">
+                Password Cannot be Null   
+              </div>
             </div>
 
             <div class="col-md-6">
               <label class="form-label">Date of Birth</label>
-              <input type="date" name="birthdate" class="form-control">
+              <input type="date" name="birthdate" class="form-control is-invalid">
+              <div class="invalid-feedback">
+                  Date of Birth Cannot be Null   
+              </div>
             </div>
 
             <div class="col-md-6">
               <label class="form-label">Phone Number</label>
-              <input type="text" name="phone" class="form-control" id="phoneNumber">
+              <input type="text" name="phone" class="form-control is-invalid" id="phoneNumber">
+              <div class="invalid-feedback">
+                  Phone Cannot be Null   
+              </div>
             </div>
 
             <div class="col-md-6">
               <label class="form-label">Photo</label>
-              <input type="file" name="photo" class="form-control" placeholder="Input Your Photo">
+              <input type="file" name="photo" class="form-control is-invalid" placeholder="Input Your Photo">
+              <div class="invalid-feedback">
+                  Phone Cannot be Null   
+              </div>
             </div>
           </div>
 
