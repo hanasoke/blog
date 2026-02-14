8a57a2d98f1b75f9cf3e34889d1377a7a250fb98
hanasbp <hanasoke@gmail.com>
2026-02-14 23:21:25 +0700

    update RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,6 +73,8 @@ class RegisterController extends Controller
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'phone' => $data['phone'],
+            'birthdate' => $data['birthdate'],
         ]);
     }
 }
