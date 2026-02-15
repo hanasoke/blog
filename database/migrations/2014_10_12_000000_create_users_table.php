62cb18f6c01eb919c3baae00d4e19a48b07bcab0
hanasbp <hanasoke@gmail.com>
2026-02-15 18:26:42 +0700

    updated create_user_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,34 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('username');
+            $table->string('username')->unique(); // tambahkan unique
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('photo');
-            $table->string('roles');
-            $table->string('access');
-            $table->string('birthdate');
+            $table->string('photo')->nullable(); // ubah jadi nullable
+            $table->string('phone');
+            $table->date('birthdate');
+            $table->string('roles')->default('USER');
+            $table->string('access')->default('FREE');
             $table->rememberToken();
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    
     public function down()
     {
         Schema::dropIfExists('users');
