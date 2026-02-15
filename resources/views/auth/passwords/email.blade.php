6cab9928ad6057bd21a9509a7bad922cebbb47bd
hanasbp <hanasoke@gmail.com>
2026-02-15 17:18:45 +0700

    updated email & verify pages

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,45 +1,34 @@
-@extends('layouts.app')
+@extends('layouts.sign.template')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="container mt-5">
+    <div class="row g-4 justify-content-center">
+        <div class="col-md-5">
+            <div class="card auth-card p-4 h-100">
+                <h3 class="fw-bold mb-1 text-center">{{ __('Reset Password') }}</h3>
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="email" class="form-label">{{ __('E-Mail Address') }}</label>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Input Your Email" autofocus>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">
+                        {{ __('Send Password Reset Link') }}
+                    </button>
+                </form>
             </div>
         </div>
     </div>
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -11,14 +11,13 @@
                     <a href="{{ route('register') }}" class="btn btn-primary w-50">Register</a>
                     <a href="{{ route('login') }}" class="btn btn-light w-50">Login</a>
                 </div>
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                        {{ __('If you did not receive the email') }},
-                        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                            @csrf
-                            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                        </form>
-                    @endsection
-                </div>
+                {{ __('Before proceeding, please check your email for a verification link.') }}
+                {{ __('If you did not receive the email') }},
+                <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                    @csrf
+                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                </form>
         </div>
     </div>
 </div>
+@endsection
