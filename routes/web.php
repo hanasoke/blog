4bef878fd5237779da79e8fa087786b797166e52
hanasbp <hanasoke@gmail.com>
2026-02-17 14:19:36 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,9 @@ Route::prefix('admin')
                 Route::get('/', 'DashboardController@index')
                         ->name('dashboard');
         });
-Route::get('/home', 'DashboardController@index')
+Route::prefix('home')
         ->namespace('User')
+        ->middleware(['auth', 'verified'])
         ->group(function() {
                 Route::get('/', 'DashboardController@index')
                         ->name('dashboard');
