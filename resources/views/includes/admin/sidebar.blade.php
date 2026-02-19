4de22b6586ac7bd88bda6f5b1b24aad872c15a78
hanasbp <hanasoke@gmail.com>
2026-02-19 07:43:33 +0700

    update admin page

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -37,8 +37,8 @@
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
+                <a class="collapse-item" href="#">Buttons</a>
+                <a class="collapse-item" href="#">Cards</a>
             </div>
         </div>
     </li>
@@ -54,10 +54,10 @@
             data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
+                <a class="collapse-item" href="#">Colors</a>
+                <a class="collapse-item" href="#">Borders</a>
+                <a class="collapse-item" href="#">Animations</a>
+                <a class="collapse-item" href="#">Other</a>
             </div>
         </div>
     </li>
@@ -72,14 +72,14 @@
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
-        <a class="nav-link" href="charts.html">
+        <a class="nav-link" href="#">
             <i class="fas fa-fw fa-chart-area"></i>
             <span>Charts</span></a>
     </li>
 
     <!-- Nav Item - Tables -->
     <li class="nav-item">
-        <a class="nav-link" href="tables.html">
+        <a class="nav-link" href="#">
             <i class="fas fa-fw fa-table"></i>
             <span>Tables</span></a>
     </li>
