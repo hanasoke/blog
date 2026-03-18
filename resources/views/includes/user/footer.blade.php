182611ebc66964e67b1fafd5f9514905ebc4aaa5
hanasbp <hanasoke@gmail.com>
2026-03-18 16:11:29 +0700

    updated user footer

---
+++ resources/views/includes/user/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="footer py-5">
+    <div class="container">
+        <hr class="my-4">
+        <div class="text-center">
+            <p class="text-dark-50 mb-0">&copy; 2026 Foundry. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
