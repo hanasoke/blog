b36e33c8b58d4f8c137d1a7330d88b15ab6ca17f
hanasbp <hanasoke@gmail.com>
2026-03-19 23:00:55 +0700

    updated sidebar & template blade.php for admin

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -24,7 +24,7 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Interface
+        Blog Menu 
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
@@ -32,18 +32,19 @@
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
             aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
+            <span>Blogs</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="#">Buttons</a>
-                <a class="collapse-item" href="#">Cards</a>
+                <h6 class="collapse-header">Blogs Component</h6>
+                <a class="collapse-item" href="#">Added A Blog</a>
+                <a class="collapse-item" href="#">Blogs Data View</a>
+                <a class="collapse-item" href="#">Access Blog Article</a>
             </div>
         </div>
     </li>
 
-    <!-- Nav Item - Utilities Collapse Menu -->
+    <!-- Nav Item - Users Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
             aria-expanded="true" aria-controls="collapseUtilities">
--- resources/views/layouts/admin/template.blade.php
+++ resources/views/layouts/admin/template.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Admin Blogs</title>
+    <title>Admin Panel</title>
 
     @include('includes.admin.style')
 
