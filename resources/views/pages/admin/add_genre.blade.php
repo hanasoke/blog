31ec87284f69480515c5ff568e639a2fd811bdd4
hanasbp <hanasoke@gmail.com>
2026-03-23 17:16:58 +0700

    updated edit_genre.blade.php

--- resources/views/pages/admin/add_genre.blade.php
+++ resources/views/pages/admin/add_genre.blade.php
@@ -14,13 +14,22 @@
             <h6 class="font-weight-bold text-primary m-0 float-left">Add Genre</h6>
         </div>
         <div class="card-body">
-            <form>
+            <form action="{{ route('store_genre') }}" method="POST">
+                @csrf 
                 <div class="form-group">
                     <label for="blog_title">Genre Name</label>
-                    <input type="text" class="form-control is-invalid" id="blog_title" placeholder="Input Blog Title" aria-describedby="validationServer03Feedback">
-                    <div id="validationServer03Feedback" class="invalid-feedback">
-                        Please Type Genre Name
-                    </div>
+                    <input 
+                        type="text"
+                        name="name" 
+                        class="form-control @error('name') is-invalid @enderror" 
+                        value="{{ old('name') }}" 
+                        id="blog_title">
+
+                    @error('name')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror 
                 </div>
                 <button class="btn btn-success btn-lg btn-block">Submit</button>
             </form>
