f211917ec1e22b9062466dd2e0647416aa5dc30d
hanasbp <hanasoke@gmail.com>
2026-03-29 16:26:06 +0700

    updated verify.blade.php

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -7,10 +7,6 @@
         <div class="col-md-5">
             <div class="card auth-card p-4 h-100">
                 <h3 class="fw-bold mb-1 text-center">Check Your Email Now Before Login</h3>
-                <div class="d-flex gap-2 mt-4">
-                    <a href="{{ route('register') }}" class="btn btn-primary w-50">Register</a>
-                    <a href="{{ route('login') }}" class="btn btn-light w-50">Login</a>
-                </div>
                 {{ __('Before proceeding, please check your email for a verification link.') }}
                 {{ __('If you did not receive the email') }},
                 <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
