2d0495d663789c53e440f69543abdb36167d3726
hanasbp <hanasoke@gmail.com>
2026-04-01 11:54:46 +0700

    updated BlogController.php

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -4,15 +4,23 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller; 
 use Illuminate\Http\Request;
+use App\Blog;
+use App\Genre;
+use App\Source;
+use Illuminate\Support\Facades\Storage;
 
 class BlogController extends Controller 
 {
     public function blogs_data() {
-        return view('pages.admin.blog.blogs_data');
+        $blogs = Blog::with(['genre', 'source'])->orderBy('created_at', 'desc')->get();
+
+        return view('pages.admin.blog.blogs_data', compact('blogs'));
     }
 
     public function add_blog() {
-        return view('pages.admin.blog.add_blog');
+        $genres = Genre::orderBy('name')->get();
+        $sources = Source::orderBy('name')->get();
+        return view('pages.admin.blog.add_blog', compact('genres', 'sources'));
     }
 
     public function edit_blog() {
