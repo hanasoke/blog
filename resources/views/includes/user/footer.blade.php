15dcba84db7e4589eec16767df4d332a514b7a92
hanasbp <hanasoke@gmail.com>
2026-04-14 08:59:59 +0700

    updated admin & user footers

--- resources/views/includes/user/footer.blade.php
+++ resources/views/includes/user/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <hr class="my-4">
         <div class="text-center">
-            <p class="text-dark-50 mb-0">&copy; 2026 Foundry. All rights reserved.</p>
+            <p class="text-dark-50 mb-0">Copyright &copy; HAN BLOG {{ date('Y') }}</p>
         </div>
     </div>
 </footer>
