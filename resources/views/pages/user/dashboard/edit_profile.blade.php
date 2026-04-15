1c5f93c53b927f828f7324f0fca6c41dce655d8f
hanasbp <hanasoke@gmail.com>
2026-04-15 23:13:36 +0700

    updated edit_profile.blade.php

---
+++ resources/views/pages/user/dashboard/edit_profile.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.user.template')
+
+@section('content')
+  <!-- BLOG LIST -->
+  <section class="py-5">
+    <div class="container">
+      <div class="row g-4">
+        <div class="card mb-3">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="..." class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Edit Profile</h5>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+@endsection
