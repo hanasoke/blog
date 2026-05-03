5b5c352dbfb754f6037e901245f08a8a8ce9268b
hanasbp <hanasoke@gmail.com>
2026-05-03 18:31:37 +0700

    updated access_blog mainpage

--- resources/views/pages/admin/access_blogs/index.blade.php
+++ resources/views/pages/admin/access_blogs/index.blade.php
@@ -43,134 +43,66 @@
                     <thead>
                         <tr>
                             <th width="10" class="text-center">NO</th>
-                            <th>Judul Blog</th>
+                            <th>Blog Title</th>
                             <th class="text-center">Access</th>
+                            <th class="text-center">Price</th>
                             <th>Created At</th>
-                            <th width="30" class="text-center">Action</th>
+                            <th width="100" class="text-center">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td class="text-center">
-                                1
-                            </td>
-                            <td>
-                                #
-                            </td>
-                            <td class="text-center">
-                                <span class="badge p-2">#</span>
-                            </td>
-                            <td>
-                                #
-                            </td>
-                            <td class="text-center">
-                                <div class="btn-group" role="group" aria-label="Basic example">
-                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">
-                                        <i class="fas fa-trash fa-sm"></i>
-                                    </button>
-                                    <button type="button" class="btn btn-info"  data-toggle="modal" data-target="#viewModal"><i class="fas fa-eye fa-sm text-white-100"></i></button>
-                                    <a href="#" class="btn btn-success">
-                                        <i class="fas fa-edit fa-sm text-white-100"></i>
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
-
-                        <!-- Detail Modal -->
-                        <div class="modal fade" id="viewModal" tabindex="-1" aria-labelledby="viewModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-lg">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="viewModalLabel">
-                                            View Detail <b>Blog Title</b> Article
-                                        </h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
+                        @forelse($accessBlogs as $index => $access)
+                            <tr>
+                                <td class="text-center">
+                                    1
+                                </td>
+                                <td>
+                                    <strong>{{ $access->blog->title ?? 'Unknown' }}</strong>
+                                </td>
+                                <td class="text-center">
+                                    @php 
+                                        $badgeClass = '';
+                                        switch($access->member->name ?? '') {
+                                            case 'BASIC': 
+                                                $badgeClass = 'badge-info';
+                                                break;
+                                            case 'PREMIUM': 
+                                                $badgeClass = 'badge-warning';
+                                                break;
+                                            case 'VIP':
+                                                $badgeClass = 'badge-danger';
+                                                break;
+                                            default:
+                                                $badgeClass = 'badge-secondary';
+                                        }
+                                    @endphp 
+                                    <span class="badge p-2 {{ $badgeClass }}">
+                                        {{ $access->member->name ?? 'N/A' }}
+                                    </span>
+                                </td>
+                                <td>
+                                    Rp {{ number_format($access->member->price ?? 0, 0, ',', '.') }}
+                                </td>
+                                <td>
+                                    {{ $access->created_at ? $access->created_at->format('d M Y H:i') : 'N/A' }}
+                                </td>
+                                <td class="text-center">
+                                    <div class="btn-group" role="group" aria-label="Basic example">
+                                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">
+                                            <i class="fas fa-trash fa-sm"></i>
                                         </button>
+                                        <button type="button" class="btn btn-info"  data-toggle="modal" data-target="#viewModal"><i class="fas fa-eye fa-sm text-white-100"></i></button>
+                                        <a href="#" class="btn btn-success">
+                                            <i class="fas fa-edit fa-sm text-white-100"></i>
+                                        </a>
                                     </div>
-                                    <div class="modal-body">
-                                        <div class="form-group row">
-                                            <label class="col-sm-4 col-form-label">Blog Title</label>
-                                            <div class="col-sm-8">
-                                                <p class="form-control-plaintext">
-                                                    Blog Title
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-4 col-form-label">Thumbnail</label>
-                                            <div class="col-sm-8">
-                                                <img src="#" class="img-thumbnail" alt="#" width="200">
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-4 col-form-label">Source</label>
-                                            <div class="col-sm-8">
-                                                <p class="form-control-plaintext">
-                                                    #
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-4 col-form-label">Genre</label>
-                                            <div class="col-sm-8">
-                                                <p class="form-control-plaintext">
-                                                    #
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-4 col-form-label">Access</label>
-                                            <div class="col-sm-8">
-                                                <p class="form-control-plaintext">
-                                                    #
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-sm-4 col-form-label">Published</label>
-                                            <div class="col-sm-8">
-                                                <p class="form-control-plaintext">
-                                                    #
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Delete Modal -->
-                        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="deleteModalLabel">
-                                            Delete <b>#</b> Article
-                                        </h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <p>
-                                            Are you sure want to delete access <b>Access Name</b> for blog <b> Blog Title</b>
-                                        </p>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <button type="button" class="btn btn-danger">Delete</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <tr>
-                            <td colspan="5" class="text-center">No access blogs found.</td>
-                        </tr>
+                                </td>
+                            </tr>
+                        @empty 
+                            <tr>
+                                <td colspan="6" class="text-center">No access blogs found.</td>
+                            </tr>
+                        @endforelse
                     </tbody>
                 </table>
             </div>
@@ -178,4 +110,96 @@
     </div>
 </div>
 <!-- /.container-fluid -->
+
+<!-- Detail Modal -->
+<div class="modal fade" id="viewModal" tabindex="-1" aria-labelledby="viewModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="viewModalLabel">
+                    View Detail <b>Blog Title</b> Article
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label">Blog Title</label>
+                    <div class="col-sm-8">
+                        <p class="form-control-plaintext">
+                            Blog Title
+                        </p>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label">Thumbnail</label>
+                    <div class="col-sm-8">
+                        <img src="#" class="img-thumbnail" alt="#" width="200">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label">Source</label>
+                    <div class="col-sm-8">
+                        <p class="form-control-plaintext">
+                            #
+                        </p>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label">Genre</label>
+                    <div class="col-sm-8">
+                        <p class="form-control-plaintext">
+                            #
+                        </p>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label">Access</label>
+                    <div class="col-sm-8">
+                        <p class="form-control-plaintext">
+                            #
+                        </p>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label">Published</label>
+                    <div class="col-sm-8">
+                        <p class="form-control-plaintext">
+                            #
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Delete Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteModalLabel">
+                    Delete <b>#</b> Article
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    Are you sure want to delete access <b>Access Name</b> for blog <b> Blog Title</b>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection 
