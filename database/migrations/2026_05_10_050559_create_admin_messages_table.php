3f45b343d610f93b10228d460dec780e67b73328
hanasbp <hanasoke@gmail.com>
2026-05-10 12:21:25 +0700

    added admin messages table

---
+++ database/migrations/2026_05_10_050559_create_admin_messages_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('transaction_id');
+            $table->string('message');
+            $table->timestamps();
+
+            // Add foreign key constraint 
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDeleter('restrict')
+                ->onUpdate('cascade');
+
+            // Add foreign key constraint 
+            $table->foreign('transaction_id')
+                ->references('id')
+                ->on('transactions')
+                ->onDeleter('restrict')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_messages');
+    }
+}
