594003b01e4b230e735cac1573f2e6f414ab7ae5
hanasbp <hanasoke@gmail.com>
2026-05-12 18:45:26 +0700

    updated pending mainpage

--- resources/views/pages/admin/transaction/pending.blade.php
+++ resources/views/pages/admin/transaction/pending.blade.php
@@ -80,26 +80,26 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="viewModalLabel"><b>#</b></h5>
+                <h5 class="modal-title" id="viewModalLabel"><b>View Pending Transaction</b></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <div class="form-group row">
-                    <label class="col-sm-4 col-form-label">Blog Title</label>
+                    <label class="col-sm-4 col-form-label">Username</label>
                     <div class="col-sm-8">
                         <p class="form-control-plaintext" id="title">#</p>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-4 col-form-label">Published</label>
+                    <label class="col-sm-4 col-form-label">Email</label>
                     <div class="col-sm-8">
                         <p class="form-control-plaintext">#</p>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="source" class="col-sm-4 col-form-label">Source</label>
+                    <label for="source" class="col-sm-4 col-form-label">Wallet Name</label>
                     <div class="col-sm-8">
                         <p class="form-control-plaintext">#</p>
                     </div>
@@ -113,31 +113,29 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="thumbnail" class="col-sm-4 col-form-label">Thumbnail</label>
+                    <label for="thumbnail" class="col-sm-4 col-form-label">Payment Proof</label>
                     <div class="col-sm-8">
                         <img src="#" class="img-thumbnail" alt="#" width="200" id="thumbnail" >
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="image_2" class="col-sm-4 col-form-label">Image 2</label>
+                    <label class="col-sm-4 col-form-label">Account Number</label>
                     <div class="col-sm-8">
-                        <img src="#" class="img-thumbnail" alt="#" width="200" id="image_2">
+                        <p class="form-control-plaintext">
+                            #
+                        </p>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="image_3" class="col-sm-4 col-form-label">Image 3</label>
+                    <label class="col-sm-4 col-form-label">Requested Member</label>
                     <div class="col-sm-8">
-                        <img src="#" class="img-thumbnail" alt="#" width="200" id="image_3">
+                        <p class="form-control-plaintext">
+                            #
+                        </p>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <div class="col-sm-4 col-form-label font-weight-bold">Description</div>
-                    <div class="col-sm-8">
-                        <p class="form-control-plaintext">#</p>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-4 col-form-label">Author</label>
+                    <div class="col-sm-4 col-form-label font-weight-bold">Status</div>
                     <div class="col-sm-8">
                         <p class="form-control-plaintext">#</p>
                     </div>
@@ -189,6 +187,14 @@
                     <div class="col-sm-4 col-form-label">Account Number</div>
                     <div class="col-sm-8">
                         <p class="form-control-plaintext">#</p>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-4 col-form-label">Requested Member</label>
+                    <div class="col-sm-8">
+                        <p class="form-control-plaintext">
+                            #
+                        </p>
                     </div>
                 </div>
                 <div class="form-group row">
