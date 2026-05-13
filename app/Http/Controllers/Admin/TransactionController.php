58a1088e55c30bd521acbc52b13b5bd829592a76
hanasbp <hanasoke@gmail.com>
2026-05-13 10:04:28 +0700

    updated pending_transaction function in TransactionController.php

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use App\Transaction;
 
 class TransactionController extends Controller 
 {
     public function pending_transaction() {
-        return view('pages.admin.transaction.pending');
+
+        $transactions = Transaction::with(['user', 'member', 'payment'])
+            ->where('status', Transaction::STATUS_PENDING)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return view('pages.admin.transaction.pending', compact('transactions'));
     }
 
     public function cancel_transaction() {
