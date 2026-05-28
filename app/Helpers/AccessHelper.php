980d7d73e80da1c32fbeefa4b28cbe1d297589e2
hanasbp <hanasoke@gmail.com>
2026-05-28 14:38:34 +0700

    updated AccessHelper.php

--- app/Helpers/AccessHelper.php
+++ app/Helpers/AccessHelper.php
@@ -4,3 +4,42 @@ namespace App\Helpers;
 
 use App\Blog;
 use App\User;
+
+class AccessHelper 
+{
+    // Access level hierarchy
+    const ACCESS_LEVELS = [
+        'FREE' => 0,
+        'BASIC' => 1,
+        'PREMIUM' => 2,
+        'VIP' => 3
+    ];
+
+    // Check if user can access a blog 
+    public static function canAccessBlog(User $user, Blog $blog)
+    {
+        // VIP can access everything
+        if($user->access == 'VIP') {
+            return true; 
+        }
+
+        // No access restriction 
+        if(!$blog->access || !$blog->access->member) {
+            return true;
+        }
+
+        $userLevel = self::ACCESS_LEVELS[$user->access] ?? 0;
+        $requiredLevel = self::ACCESS_LEVELS[$blog->access->member->name] ?? 0;
+
+        return $userLevel >= $requiredLevel;
+    }
+
+    // Get required access level for a blog 
+    public static function getRequiredLevel(Blog $blog)
+    {
+        if($blog->access && $blog->access->member) {
+            return $blog->access->member->name;
+        }
+        return 'FREE';
+    }
+}
